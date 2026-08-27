d61b3c9bbeec0612985ea5bc5a023eb501e32d03
phanchuc123 <anhaikhoilop520182019@gmail.com>
2026-08-27 09:57:53 +0700

    update font

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -6,10 +6,10 @@ export default function Header() {
                     Smart Trip Đà Nẵng
                 </div>
                 <div className="flex space-x-6 gap-x-8">
-                    <a href="#" className="text-[#006971] hover:font-semibold text-sm">Home</a>
-                    <a href="#" className="text-[#006971] hover:font-semibold text-sm">Explore</a>
-                    <a href="#" className="text-[#006971] hover:font-semibold text-sm">Trip</a>
-                    <a href="#" className="text-[#006971] hover:font-semibold text-sm">Suggestions</a>
+                    <a href="#" className="text-[#006971] hover:font-semibold text-sm font-bold">Home</a>
+                    <a href="#" className="text-[#006971] hover:font-semibold text-sm font-bold">Explore</a>
+                    <a href="#" className="text-[#006971] hover:font-semibold text-sm font-bold">Trip</a>
+                    <a href="#" className="text-[#006971] hover:font-semibold text-sm font-bold">Suggestions</a>
                 </div>
                 <div>
                     <i className="fa-solid fa-user text-[#006971] text-lg"></i>
